bfef35eb6d621d8e2ec5d780b1966395b2f654dd
Duy <zzxxcc2056@gmail.com>
2019-10-21 22:10:11 +0700

    try

--- app.py
+++ app.py
@@ -11,24 +11,24 @@ from src.auth import Login, SignUp
 from src.admin import GetUsersInfo
 # -----------------------------------------------------------------------------
 app = Flask(__name__)
-#for develop
+# for develop
 app.debug = False
 api = Api(app)
 CORS(app)
 
-#for develop
+# for develop
 logging.basicConfig(level = logging.INFO)
-#book
+# book
 api.add_resource(GetBook, "/GetBook/<int:bookId>")
 api.add_resource(GetBooks, "/GetBooks/<int:page>")
 api.add_resource(GetBooksWithName, "/GetBooksName/<string:name>/<int:page>")
 api.add_resource(GetBooksWithSubject, "/GetBooksSubject/<string:subject>/<int:page>")
 api.add_resource(GetBooksWithAuthor, "/GetBooksAuthor/<string:author>/<int:page>")
 
-#auth
+# auth
 api.add_resource(Login, "/Login")
 api.add_resource(SignUp, "/SignUp")
-#admin
+# admin
 api.add_resource(GetUsersInfo, "/Admin/GetUsers")
 
 if __name__ == "__main__":
