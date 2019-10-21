bcc8c10b692f2850e67ab85d728912d1b6239c11
Duy <zzxxcc2056@gmail.com>
2019-10-21 22:11:51 +0700

    add admin api

---
+++ src/admin.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from flask_restful import Resource
+from flask import request
+from uuid import uuid4
+import logging
+# -----------------------------------------------------------------------------
+from src.configs import db
+from src.utils import isJsonValid, getToken
+# -----------------------------------------------------------------------------
+
+class GetUsersInfo(Resource):
+	def post(self):
+		try:
+			json = request.get_json()
+			valid = ['token']
+			# invalid json
+			if not isJsonValid(valid, json):
+				return 'json invalid', 400
+			# get token info
+			token = getToken(json['token'])
+			# token expired or not admin
+			if token == None or token['role'] != 'admin':
+				return '', 403
+			# get all user info
+			users = []
+			for user in db.account.find():
+				if 'email' not in user:
+					user['email'] = ''
+				users.append(
+					{
+						'username': user['_id'],
+						'email': user['email'],
+						'role': user['role'],
+						'borrowed': user['borrowed']
+					}
+				)
+			return {'users': users}, 200
+				
+		except Exception as e:
+			logging.info('error GetAllUserInfo: %s',e)
+		return '', 400
