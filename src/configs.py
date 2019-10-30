9a2bd75bd6dadf71ab6dbc978e901cfaf9e5a675
Duy <zzxxcc2056@gmail.com>
2019-10-31 01:00:04 +0700

    change mongoDB pass

--- src/configs.py
+++ src/configs.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 # -----------------------------------------------------------------------------
 # mongoDB connection string
-mongodb = 'mongodb+srv://libary:ecyfAnTIz058VdrP@cluster0-uuoo5.gcp.mongodb.net/test?retryWrites=true&w=majority'
+mongodb = 'mongodb+srv://libary:3AvmVMAqu2Xs2v1n@cluster0-uuoo5.gcp.mongodb.net/test?retryWrites=true&w=majority'
 client = MongoClient(mongodb)
 db = client.library
 
