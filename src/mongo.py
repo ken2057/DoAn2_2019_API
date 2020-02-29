54eafe803f0e4d856b121f1fd1ee35640dff205b
Duy <zzxxcc2056@gmail.com>
2020-03-01 01:15:45 +0700

    add db

--- src/mongo.py
+++ src/mongo.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 # -----------------------------------------------------------------------------
 # mongoDB connection string
-mongodb = ''
+mongodb = 'mongodb+srv://libary:3AvmVMAqu2Xs2v1n@cluster0-uuoo5.gcp.mongodb.net/test?retryWrites=true&w=majority'
 client = MongoClient(mongodb)
 db = client.library
